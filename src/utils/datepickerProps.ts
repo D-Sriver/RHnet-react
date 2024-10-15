96a303a94ca6d98dfc6138dd743181253bcca07d
D.Sriver <duez.s@proton.me>
2024-10-15 03:08:16 +0200

    ⬆️ Update last version for date picker & add props

--- src/utils/datepickerProps.ts
+++ src/utils/datepickerProps.ts
@@ -1,12 +1,26 @@
 export const PropsPicker = {
-	colorPrimary: '#4caf50',
-	colorSecondary: '#ffffff',
-	colorTertiary: '#333333',
-	colorBackgroundField: 'rgb(209 213 219 / 0.2)',
-	colorBorderField: 'rgb(209 213 219 / 0.5)',
-	placeholder: 'Select a date',
+	// general color
+	colorPrimary: 'rgb(132 204 22)',
+	colorSecondary: 'rgba(255, 255, 255, 0.1)',
+	colorTertiary: 'rgba(255, 255, 255, 0.9)',
+
+	// field options
+	colorBackgroundField: 'transparent',
+	colorBorderField: 'rgba(255, 255, 255, 0.5)',
+	colorTextField: 'rgba(255, 255, 255, 0.8)',
+	blurBackground: 5,
+
+	// placeholder options
+	placeholder: 'Select Date',
 	placeholderColor: 'rgba(255, 255, 255, 0.9)',
+
+	// select color options
+	colorBackgroundSelect: 'rgba(0, 0, 0, 0.3)',
+
+	// icon color options
+	colorIcon: 'rgba(255, 255, 255, 0.5)',
+
+	// year options
 	yearRangeStart: 100,
 	yearRangeEnd: 10,
-	colorIcon: 'rgba(255, 255, 255, 0.5)',
 };
